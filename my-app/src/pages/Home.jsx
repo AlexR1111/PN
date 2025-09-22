825171dbcf5122eb4482b5646cf8b9d119972ac0
AlexR1111 <148652561+AlexR1111@users.noreply.github.com>
2025-09-22 09:35:01 +0200

    Update Home.jsx

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -16,15 +16,20 @@ const Home = () => (
         <p>Ob historische Gewandung oder moderne Upcycling-Ideen – ich verwandle Stoffe in individuelle Lieblingsstücke. Änderungswünsche? Kein Problem – ich passe alles nach deinen Vorstellungen an.</p>
         <VorschauKarten/>
         <SidebarPreview/>
-        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+        <p>Bei mir gibt es von Hand gefertigte und bestickte Gewandungen nach
+        Wunsch des Kunden vom Mittelalter bis zur Renaissance, für Fantasy und
+        Rollenspiel.
 
-Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+        Sie können bestellt und individuell auf bestehende Outfits oder ein neues
+        Ensemble passend angefertigt werden.
+        Von mir hergestellte Gewandungen und Zubehör sind immer Unikate, die
+        von mir mit Herzblut ( Nadelstiche ) hergestellt werden.
 
-Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+        Die Träger können sich deshalb immer eines Einzelstückes sicher sein!
+        Ich fertige grundsätzlich auf Wunsch des Kunden und versuche, möglichst
+        nahe an Vorlagen ranzukommen
 
-Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
-
-Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis. </p>
+        Die Fotos sind Beispiele meiner Arbeiten. </p>
     </motion.div>
 );
 
